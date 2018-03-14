45c412057afaafcc20551ce382c8a0a1b98f4a82
Luis Garcia <garcia@yahoo.com>
2018-03-13 21:22:45 -0600

    Add show action

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,4 +3,14 @@ class Api::V1::ItemsController < ApplicationController
   def index
     render json: Item.all
   end
+  def create
+    render json: Item.create(item_params)
+  end
+
+  private
+
+    def item_params
+      params.require(:item).permit(:name, :description)
+    end
+
 end
